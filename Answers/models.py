ed3d8c35cabbf45b8a5e83e1ec088fb36adce485
Jhonbeltran <jbeltranleon@gmail.com>
2017-03-11 15:24:06 -0500

    Cambio Change pass answer a integer y ejecuto las migraciones

--- Answers/models.py
+++ Answers/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Answer(models.Model):
-	change_pass_answer = models.CharField(max_length=255)
+	change_pass_answer = models.IntegerField()
 	relation_answer = models.CharField(max_length=255)
 	personal_answer = models.CharField(max_length=255)
 	pay_method_answer = models.CharField(max_length=255)
