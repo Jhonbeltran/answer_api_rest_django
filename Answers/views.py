21e56ec5e2dca842dd6b092111d6965ff23c68e3
Jhonbeltran <jbeltranleon@gmail.com>
2017-03-11 14:25:25 -0500

    Creo la vista que va a traer lo objetos de la api

--- Answers/views.py
+++ Answers/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Answer
+from .serializers import AnswerSerializer
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+class AnswerList(generics.ListCreateAPIView):
+	queryset = Answer.objects.all()
+	serializer_class = AnswerSerializer
+
+	def get_object(self):
+		queryset = self.get_queryset()
+		obj = get_object_or_404(
+			queryset,
+			pk = self.kwargs['pk']
+			)
+		return obj
